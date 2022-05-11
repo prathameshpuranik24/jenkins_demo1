4b10c2e7718269f52443a430616a0891e5dcf292
Prathamesh Puranik <85353774+prathameshpuranik24@users.noreply.github.com>
2022-05-11 20:48:54 +0530

    Update MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,8 +1,5 @@
 public class MyFirstJavaProgram {
-
-   
    public static void main(String []args) {
       System.out.println("Hello World"); 
-      System.out.println("Hello I am Prathamesh"); 
    }
 }
